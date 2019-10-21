e5bb8bf3d5fb7fd09cd4dcc479fb163abba6c389
PfanoKhampha <55394528+PfanoKhampha@users.noreply.github.com>
2019-10-21 13:16:05 +0200

    Merge

--- js/watch-list.js
+++ js/watch-list.js
@@ -16,5 +16,20 @@ $(function(){
     });
     
     
+    function createContainers (img_src, movieName, runTime, director, releaseDate){
+        
+        $().append("<div class='filmboxleft'><img src=" + img_src "><h3 id='movie-title'></h3><h5 id='runtime'>Run Time:</h5><h5 id='director'>Director:</h5><h5 id='release-date'>Release Date:</h5><div class="button">WATCH NOW</div><div class="remove-item"><h6>REMOVE FROM WATCHLIST</h6></div>
+            </div>
+        
+     
+            
+                        
+
+        )
+        
+        
+    }
+    
+    
     
 });
