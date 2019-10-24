81a826b1d25d968d8617e5d2a078eae058e9131a
Nadine Pace <190043@virtualwindow.co.za>
2019-10-24 07:09:59 +0200

    Details page updates

--- js/apiRequest.js
+++ js/apiRequest.js
@@ -1,10 +1,52 @@
 $(function(){
     
-    console.log("api js linked");
+    var recommendedMovies = [
+        "SpiderMan",
+        "Mike",
+        "Nadine"
+    ];
+    
+    
+    flikPoster = "Hello"
+    flikTitle = "Hello"
+    flikGenre = "Hello"
+    flikTime = "Hello"
+    flikDirector = "Hello"
+    flikDate = "Hello"
+    
+    // For Loop to run through above array 
+    
+    //for(i = 0; i < recommendedMovies.length; i++){
+        //console.log(recommendedMovies[i]);
+        
+        //$(".recommended-row").append(
+            //"<div class='flip-card col-lg-2' style='background-color:  #072446;'><div class='flip-card-inner'><div class='flip-card-front'><div class='movie-card' style='background-color:  #072446;'><div class='movie-poster'>" + flikPoster + "</div><div class='splash-info'><div class='splash-row-one'><h3>" + flikTitle + "</h3></div><p class='show-genre'>" + flikGenre + "</p><div class='movie-time d-none d-lg-block'><p class='show-time'>" + flikTime + "</p><img src='../img/UI/Clock.svg'></div></div></div></div><div class='flip-card-back'><h2>" + flikTitle + "</h2><p>RUNTIME:</p><h5>" + flikTime + "</h5><p>DIRECTOR:</p><h5>" + flikDirector + "</h><p>RELEASE DATE:</p><h5>" + flikDate + "</h5><p>GENRE</p><h5>" + flikGenre + "</h5><a href='watch-list.html'><div class='flip-watchlist'><p>ADD TO WATCHLIST</p></div></a><a href='details-page.html'><div class='flip-details'><p>VIEW MORE DETAILS</p></div></a></div></div></div>"
+        
+       // );
+        
+        
+   // }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    console.log(recommendedMovies);
+    
+
+    
+    
+    
+    
+    
     
     var apiRequest = new XMLHttpRequest();
     
-    apiRequest.open('GET', 'http://www.omdbapi.com/?apikey=3b5ca08c&', 'true');
+    apiRequest.open("http://www.omdbapi.com/?apikey=3b5ca08c&t=");
     
     apiRequest.send();
     
