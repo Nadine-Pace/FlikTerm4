384341de49c94618b3ce46af8be027172c7f3235
Jeandre <190025@virtualwindow.co.za>
2019-10-25 10:56:35 +0200

    Mobile First Design

--- js/index.js
+++ js/index.js
@@ -1,26 +1,8 @@
-/*$(".container-1").css({"height": (($(window).height()))+"px"});
- 
-$(window).on("resize", function(){
- $(".container-1").css({"height": (($(window).height()))+"px"});
-});
-
-
-$(".container-2").css({"height": (($(window).height()))+"px"});
- 
-$(window).on("resize", function(){
- $(".container-2").css({"height": (($(window).height()))+"px"});
-});
-
-$(".container-3").css({"height": (($(window).height()))+"px"});
- 
-$(window).on("resize", function(){
- $(".container-3").css({"height": (($(window).height()))+"px"});
-});
-
 $(document).ready(function(){
+  $(".container-1").show(1000);
     $(".container-2").hide(1000);
-      $(".container-3").hide(1000);
-    });
+    $(".container-3").hide(1000);
+});
 
 
 $(document).ready(function(){
@@ -52,6 +34,42 @@ $(document).ready(function(){
   });
 });
 });
-});*/
+});
+
+$(document).ready(function() {
+  $('.error').hide();
+  $('#submit').click(function(){
+    var name = $('#name').val();
+    var email = $('#email').val();
+
+    if(name== ''){
+      $('#name').next().show();
+      return false;
+    }
+    if(email== ''){
+      $('#email').next().show();
+      return false;
+    }
+    if(IsEmail(email)==false){
+      $('#invalid_email').show();
+      return false;
+    }
+    $.post("", $("#myform").serialize(),  function(response) {
+      $('#myform').fadeOut('slow',function(){
+      $('#correct').html(response);
+      $('#correct').fadeIn('slow');
+   });
+ });
+return false;
+});
+});
+function IsEmail(email) {
+var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+if(!regex.test(email)) {
+return false;
+}else{
+return true;
+}
+}
 
    
