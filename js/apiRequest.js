f6cc8e9bfefb76edb52c4bde17e09193336fb956
Claudia <180181@virtualwindow.co.za>
2019-10-27 21:07:17 +0200

    Dynamic js working on library page

--- js/apiRequest.js
+++ js/apiRequest.js
@@ -1,57 +1,154 @@
-$(function(){
-    
-    var recommendedMovies = [
-        "SpiderMan",
-        "Mike",
-        "Nadine", 
-        "norman"
-    ];
-    
-    
-    
-    
-    
-    for(i = 0; i < recommendedMovies.length; i++){
-    
-        var movieTitle = recommendedMovies[i];
-        
-    
-        var apiCall = 'http://www.omdbapi.com/?t=' + movieTitle +'&apikey=d90cffd6';
-        
-    
-        
-        $.getJSON(apiCall, movieTitle);
-        
-        
-        
-        
-        function filmCallBack(movieTitle){
-            
-            
-            
-    
-           var filmName = movieTitle.Title;
-            
-            
-            
-            
-            
-            console.log (filmName);
-      
-            console.log(apiCall);
-
-            $(".recommended-row").append(
-                 "<div class='col-lg-2 d-block'><div class='flip-card' style='background-color:  #072446;'><div class='flip-card-inner'><div class='flip-card-front'><div class='movie-card' style='background-color:  #072446;'><div class='movie-poster'></div><div class='splash-info'><div class='splash-row-one'><h3 class='splashTitle'>" + movieTitle + "</h3></div> <!--splash-row-one--><p class='splashGenre'>GENRE GOES HERE</p><div class='movie-time'><img src='../img/UI/Clock.svg'><p class='splashTime'>Time</p></div></div><!--splash-info--></div> <!--movie-card--></div> <!--flip-card--front--><div class='flip-card-back'><h2 class='backTitle'>Title</h2><p>RUNTIME:</p><h5 class='backTime'>time</h5><p>DIRECTOR:</p><h5 class='backDirector'>some dude</h5><p>RELEASE DATE:</p><h5 class='backRelease'>yo mama</h5><p>GENRE</p><h5 class='backGenre'>good, actually</h5><a href='watch-list.html'><div class='flip-watchlist'><p>ADD TO WATCHLIST</p></div></a><!--flip-watchlist--><a href='details-page.html'><div class='flip-details'><p>VIEW MORE DETAILS</p></div></a> <!--flip-detals--></div> <!--flip-card-back--></div> <!--flip-card-inner--></div><!--flip-card--></div>"
-           );
-            
-            
-        };
-            
-        
-    };
-     
-        
-    
-    
-
+$(function () {
+    getMovies('man', 'movie', null, '1');
 });
+
+function getMovies(keyword, type, year, page) {
+    //http://www.omdbapi.com/?apikey=d90cffd6&s=man&type=movie&r=json&page=1
+
+    var url = 'http://www.omdbapi.com/?apikey=d90cffd6';
+
+    if (keyword) {
+        url += '&s=' + keyword;
+
+        if (type) {
+            url += '&type=' + type;
+        }
+
+        if (year) {
+            url += '&y=' + year;
+        }
+
+        if (page) {
+            url += '&page=' + page;
+        }
+
+        var apiRequest = new XMLHttpRequest();
+        var container = document.getElementById('content-container');
+
+        apiRequest.open('GET', url, true);
+        apiRequest.send();
+        apiRequest.onload = function () {
+            var data = JSON.parse(this.response);
+
+            if (data) {
+                var row = document.createElement('div');
+                row.setAttribute('class', 'row container-fluid');
+
+                for (var i = 1; i <= data.Search.length; i++) {
+                    var movie = data.Search[i];
+
+                    if (movie) {
+                        var colDiv = document.createElement('div');
+                        colDiv.setAttribute('class', 'col-lg-4');
+                        colDiv.setAttribute('style', 'text-align: center;');
+
+                        var label = document.createElement('label');
+
+                        var input = document.createElement('input');
+                        input.setAttribute('type', 'checkbox');
+
+                        var cardDiv = document.createElement('div')
+                        cardDiv.setAttribute('class', 'card');
+
+                        var frontDiv = document.createElement('div');
+                        frontDiv.setAttribute('class', 'front');
+
+                        var image = document.createElement('img');
+                        image.setAttribute('src', movie.Poster);
+
+                        var backDiv = document.createElement('div');
+                        backDiv.setAttribute('class', 'back');
+
+                        var h3 = document.createElement('h3');
+                        h3.setAttribute('class', 'backTitle');
+                        createTextElement(h3, movie.Title);
+
+                        var h6 = document.createElement('h6');
+                        h6.setAttribute('class', 'backYear');
+                        createTextElement(h6, movie.Year);
+
+                        var p = document.createElement('p');
+                        p.setAttribute('class', 'backDescription');
+                        getMovieInformation(movie.imdbID, p, 'short');
+
+                        frontDiv.appendChild(image);
+                        cardDiv.appendChild(frontDiv);
+
+                        backDiv.appendChild(h3);
+                        backDiv.appendChild(h6);
+                        backDiv.appendChild(p);
+                        cardDiv.appendChild(backDiv);
+
+                        label.appendChild(input);
+                        label.appendChild(cardDiv);
+
+                        colDiv.appendChild(label);
+                        row.appendChild(colDiv);
+                        container.appendChild(row);
+
+                        if (i % 3 == 0) {
+                            row = document.createElement('div');
+                            row.setAttribute('class', 'row container-fluid');
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+function createTextElement(parent, text) {
+
+    if (parent && text) {
+        var textElement = document.createTextNode(text);
+        parent.appendChild(textElement);
+    }
+}
+
+function getMovieInformation(id, parentNode, plotType) {
+    //http://www.omdbapi.com/?apikey=d90cffd6&i=tt0316654&plot=short
+    var url = 'http://www.omdbapi.com/?apikey=d90cffd6';
+
+    if (id) {
+        url = url + '&i=' + id;
+
+        if (plotType) {
+            url = url + '&plot=' + plotType;
+        }
+
+        var apiRequest = new XMLHttpRequest();
+        apiRequest.open('GET', url, true);
+        apiRequest.send();
+        apiRequest.onload = function () {
+            var data = JSON.parse(this.response);
+
+            if (parentNode) {
+                createTextElement(parentNode, data.Plot);
+            }
+
+            return data;
+        }
+    }
+
+    return null;
+}
+    /* <div class="col-lg-3 col-md-2 col-xs-2">
+<label>
+<input type="checkbox">
+
+<div class="card">
+
+<div class="front">
+    <img
+        src="https://m.media-amazon.com/images/M/MV5BMDBhOTMxN2UtYjllYS00NWNiLWE1MzAtZjg3NmExODliMDQ0XkEyXkFqcGdeQXVyMjMxOTE0ODA@._V1_SX300.jpg">
+</div>
+
+<div class="back">
+    <h3 class="backTitle" style="padding: 5% 0%;">Title</h3>
+    <h6 class="backYear">Year</h6>
+    <p class="backDescription" style="width: 85%; text-align: center; margin: 24% auto;"></p>
+</div>
+</div>
+
+</label>
+</div> */
