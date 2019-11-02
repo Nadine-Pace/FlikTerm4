16f344ddd2f68bec98be55e37e5946057c575115
PfanoKhampha <55394528+PfanoKhampha@users.noreply.github.com>
2019-11-03 00:01:04 +0200

    working on watchlist

--- js/watch-list.js
+++ js/watch-list.js
@@ -1,111 +1,58 @@
 
 $(function () {
-
-    $('.remove-item').on('click', function () {
-        $(this).closest('.filmbox').fadeOut(600);
-    });
-<<<<<<< HEAD
     
+    var apiRequest = new XMLHttpRequest();
+
+    apiRequest.open('GET', 'http://www.omdbapi.com/?i=tt3896198&apikey=d90cffd6', true);
+
+    apiRequest.send();
     
-    var importedMovies = localStorage.getItem('storageName');
-    
-=======
-
-    // ARRAY
-    var watchList = JSON.parse(localStorage.getItem('watchList'));
-
-    if (watchList) {
-
-        console.log(watchList);
-        watchList.forEach(element => {
-            console.log(element.movieInfo.Title);
-            $('.watchListItems').append(
-                "<div class='row'><div class='filmbox col-lg-4 col-md-6 col-lg-4'><div class='picturebox'><img src='" + element.movieInfo.Poster + "'></div><div class='textbox'><h3></h3><div class='headings'><h5> RUNTIME: " + element.movieInfo.Runtime + "</h5><h5> DIRECTOR: " + element.movieInfo.Director + "</h5><h5> RELEASE DATE: " + element.movieInfo.Released + "</h5></div><div class='button'>WATCH NOW</div><div class='remove-item'><h6>REMOVE FROM WATCHLIST</h6></div></div></div></div>"
-            );
-        });
-    }
-
->>>>>>> 7174452a44980294d290ac8250500a22beb2ff30
-
-    // Get API request sructure form Cludia and paste here
-
-    // run the api here with the vlaue (movieTitle) as a varibale 
-
-    // unpack API json result, store required information as varibles 
-
-    // create title cards that show the movies using above variables
-<<<<<<< HEAD
-    
-    
-    
-    
-    
-    
-    $('.watchListItems').append(
-        "<div class='filmbox col-12 col-md-6 col-lg-4'><div class='picturebox'><img src='../img/FilmThumbnails/Titanic.jpg'></div><div class='textbox'><h3></h3><div class='hheadings'><h5> RUNTIME:</h5><h5> DIRECTOR:</h5><h5> RELEASE DATE:</h5></div><div class='button'>SEE DETAILS</div><div class='remove-item'><h6>REMOVE</h6></div></div></div>"
-    );
-    
-    
-
-  
-=======
-
-
-
-
-
-
+        apiRequest.onload = function(){
+        var watchlist = JSON.parse(localStorage.getItem('watchlist'));
+        
+                $('.watchListItems').append(
+                "<div class='row'><div class='filmbox col-12 col-md-6 col-lg-4'><div class='picturebox'><img src='" + element.movieInfo.Poster + "'></div><div class='textbox'><h3></h3><div class='headings'><h5> RUNTIME: " + element.movieInfo.Runtime + "</h5><h5> DIRECTOR: " + element.movieInfo.Director + "</h5><h5> RELEASE DATE: " + element.movieInfo.Released + "</h5></div><div class='button'>SEE DETAILS</div><div class='remove-item'><h6>REMOVE</h6></div></div></div></div>"
+            ); 
+            
+                for(i = 0; i < data.photos.length; i++){
+                 if(data.photos[i].id === storageReference){
+                    
+                    var moviePoster = data.photos[i].Poster;
+                     var movieTitle = data.photos[i].Title;
+                    var movieDirector = data.photos[i].Director;
+                     var movieGenre = data.photos[i].Genre;
+                    var movieRuntime = data.photos[i].Runtime;
+                    var movieReleaseDate = data.photos[i].Released;
+                     
    
->>>>>>> 7174452a44980294d290ac8250500a22beb2ff30
+       }} // For Statment
+        
+                       var moviePoster = localStorage.getItem('moviePoster');
+           var movieTitle = localStorage.getItem('movieTitle');
+           var movieDirector = localStorage.getItem('movieDirector');
+           var movieRuntime = localStorage.getItem('movieRuntime');
+           var movieReleaseDate = localStorage.getItem('movieReleaseDate');
+            
+            $(".picturebox").attr('src', movie.Poster);
+            $(".").text(movie.Title);
+            $("#movieDirector").text(movie.Director);
+            $("#movieRuntime").text(movie.Runtime);
+            $("#movieReleaseDate").text(movie.Released);
+            
+
+
+
+
+            
+            
+            
+            
+            
+            
+            
+            
+        }
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
