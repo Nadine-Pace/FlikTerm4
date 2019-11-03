fd4fadb0b768674a734546532eb3c3bb0c671f0c
Claudia <180181@virtualwindow.co.za>
2019-11-03 12:47:44 +0200

    library page and watchlist page functional

--- js/library.js
+++ js/library.js
@@ -1,78 +1,57 @@
-    $(function () {
 
+function doSearch() {
+    var title = $('.title-search').val();
 
+    if (title) {
+        $(".movies-row").empty();
+        getMovies(title, null, null);
+    } else {
+        alert("Please enter a title to search.");
+    }    
 
-    // var apiRequest = new XMLHttpRequest();
+}
 
-    // apiRequest.open('GET', 'http://www.omdbapi.com/?i=tt3896198&apikey=d90cffd6', true);
+function doFilter() {
+    var genre = $('.genre-dropdown option:selected').text();
+    var imdbRating = $('.imdb-score-dropdown option:selected').text();
+    var year = $('.year-search').val();
 
-    // apiRequest.send();
+    var movieArray = Array.from(movieMap.values());
 
-    // apiRequest.onload = function () {
-    //     var data = JSON.parse(this.response);
+    if (genre && "None" !== genre) {
+        movieArray = movieArray.filter((movie) => {
+            //"Comedy, Drama, Fantasy, Romance, Sci-Fi"            
+            var genres = movie.Genre.split(", ");
+            //["Comedy", "Drama", "Fantasy", "Romance", "Sci-Fi"]
 
-    //     function createContainers(moviePoster, movieTitle, movieRuntime, movieDirector, movieReleaseDate, movieGenre) {
-    //         $(".library-filter-movies").append(
-    //             <div class="library-filter-movies row col-xs-3">
-    //                 <img src="moviePoster" />
-    //                 <h3 id="movieTitle">
-    //                     <span>" + movieTitle + "</span>
-    //                 </h3>
-    //                 <p id="movieRuntime">
-    //                     <span>" + movieRuntime + "</span>
-    //                 </p>
-    //                 <p id="movieDirector">
-    //                     <span>" + movieDirector + "</span>
-    //                 </p>
-    //                 <p id="movieReleaseDate">
-    //                     <span>" + movieReleaseDate + "</span>
-    //                 </p>
-    //                 <p id="movieGenre">
-    //                     <span>" + movieGenre "</span>
-    //                 </p>
-    //             </div>);
-    //     };
+            if (genres.includes(genre.trim())) {
+                return movie;
+            }
+        });
+    }
 
+    if (imdbRating && "None" !== imdbRating) {
+        movieArray = movieArray.filter((movie) => {
 
-    //     for (i = 0; i < data.photos.length; i++) {
-    //         moviePoster = data.photos[i].Poster;
-    //         movieTitle = data.photos[i].Title;
-    //         movieDirector = data.photos[i].Director;
-    //         movieGenre = data.photos[i].Genre;
-    //         movieRuntime = data.photos[i].Runtime;
-    //         movieReleaseDate = data.photos[i].Released;
+            if (Number(movie.imdbRating) >= Number(imdbRating)) {
+                return movie;
+            }
+        });
+    }
 
-    //         createContainers(moviePoster, movieTitle, movieDirector, movieGenre, movieRuntime, movieReleaseDate);
-    //     };
+    
+    if (year) {
+        movieArray = movieArray.filter((movie) => {
+            
+            if (Number(movie.Year) >= Number(year)) {
+                return movie;
+            }
+        });
+    }
 
-    //     $(".library-filter-movies").on("click", function(){
-    //         var storageReference = +$(this).find ("#photo-id").text();
-    //         console.log(storageReference);
+    $(".movies-row").empty();
 
-    //         for(i = 0; i < data.photos.length; i++){
-    //             if(data.photos[i].id === storageReference){
-                    
-    //                 var moviePoster = data.photos[i].Poster;
-    //                 var movieTitle = data.photos[i].Title;
-    //                 var movieDirector = data.photos[i].Director;
-    //                 var movieGenre = data.photos[i].Genre;
-    //                 var movieRuntime = data.photos[i].Runtime;
-    //                 var movieReleaseDate = data.photos[i].Released;
-
-    //                 localStorage.setItem("moviePoster", moviePoster);
-    //                 localStorage.setItem("movieTitle", movieTitle);
-    //                 localStorage.setItem("movieDirector", movieDirector);
-    //                 localStorage.setItem("movieGenre", movieGenre);
-    //                 localStorage.setItem("movieRuntime", movieRuntime);
-    //                 localStorage.setItem("movieReleaseDate", movieReleaseDate);
-                    
-    //             }
-    //         }
-
-    //     });
-    // }
-
-
-
-
-});
+    movieArray.forEach(movieInfo => {
+        buildHtml(movieInfo, ".movies-row");
+    });
+}
--- js/watch-list.js
+++ js/watch-list.js
@@ -1,8 +1,24 @@
 
 $(function () {
+    $(document).on('click', '.remove-item', function () {
+        var flimBox = $(this).parent().parent();
+        var movieID = flimBox.find(".movie-id").text();
 
-    $('.remove-item').on('click', function () {
-        $(this).closest('.filmbox').fadeOut(600);
+        if (movieID) {
+            var watchlist = JSON.parse(localStorage.getItem("watchList"));
+            var listMovie = watchlist.find((movie) => {
+                return movie.movieInfo.imdbID === movieID;
+            });
+
+            if (listMovie) {
+                var newList = watchlist.filter((movie) => {
+                    return movie !== listMovie;
+                });
+
+                localStorage.setItem("watchList", JSON.stringify(newList));
+                flimBox.fadeOut(600);
+            }
+        }
     });
 
     // ARRAY
@@ -12,19 +28,11 @@ $(function () {
 
         console.log(watchList);
         watchList.forEach(element => {
-            console.log(element.movieInfo.Title);
             $('.watchListItems').append(
-                "<div class='row'><div class='filmbox col-lg-4 col-md-6 col-lg-4'><div class='picturebox'><img src='" + element.movieInfo.Poster + "'></div><div class='textbox'><h3></h3><div class='headings'><h5> RUNTIME: " + element.movieInfo.Runtime + "</h5><h5> DIRECTOR: " + element.movieInfo.Director + "</h5><h5> RELEASE DATE: " + element.movieInfo.Released + "</h5></div><div class='button'>WATCH NOW</div><div class='remove-item'><h6>REMOVE FROM WATCHLIST</h6></div></div></div></div>"
+                "<div class='row'><div class='filmbox col-lg-4 col-md-6 col-lg-4'><div class='movie-id' style='display=none;'>" + element.movieInfo.imdbID + "</div><div class='picturebox'><img src='" + element.movieInfo.Poster + "'></div><div class='textbox'><h3></h3><div class='headings'><h5> RUNTIME: " + element.movieInfo.Runtime + "</h5><h5> DIRECTOR: " + element.movieInfo.Director + "</h5><h5> RELEASE DATE: " + element.movieInfo.Released + "</h5></div><div class='button'>WATCH NOW</div><div class='remove-item'><h6>REMOVE FROM WATCHLIST</h6></div></div></div></div>"
             );
         });
     }
-
-
-    // Get API request sructure form Cludia and paste here
-
-    // run the api here with the vlaue (movieTitle) as a varibale 
-
-    // unpack API json result, store required information as varibles 
 
     // create title cards that show the movies using above variables
 
