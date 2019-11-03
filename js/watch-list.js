bf279634ab5239bb954619ef2093f5a4a6dee29d
PfanoKhampha <55394528+PfanoKhampha@users.noreply.github.com>
2019-11-03 11:16:13 +0200

    update watchlist

--- js/watch-list.js
+++ js/watch-list.js
@@ -1,4 +1,3 @@
-
 $(function () {
     
 var movieName = localStorage.getItem('detailmovieID');
@@ -6,7 +5,7 @@ var movieName = localStorage.getItem('detailmovieID');
        // Get API request structure form and put here
     
     if (movieName) {
-        var url ='http://www.omdbapi.com/?i=tt3896198&apikey=d90cffd6' + movieName;
+        var url = 'http://www.omdbapi.com/?apikey=d90cffd6&i=' + movieId;
         var apiRequest = new XMLHttpRequest();
         
         apiRequest.open('GET', url, true);
@@ -16,26 +15,33 @@ var movieName = localStorage.getItem('detailmovieID');
          // run the api here with the value (movieTitle) as a variable
             
             var movieName = JSON.parse(this.response);
-        
+            
             $('.picturebox').attr('src', movie.Poster);
             $('.picturebox').text(movie.Title);
             $('.movieDirector').text(movie.Director);
             $('.movieRuntime').text(movie.Runtime);
             $('.movieRelease').text(movie.Released);
             
+            $(".watchlist-btn-home").on("click", function(){
+                              
+     if (watchList) {
+
+        console.log(watchList);
+        watchList.forEach(element => {
+            console.log(element.movieInfo.Title);
+            $('.watchListItems').append(
+                "<div class='row'><div class='filmbox col-lg-4 col-md-6 col-lg-4'><div class='picturebox'><img src='" + element.movieInfo.Poster + "'></div><div class='textbox'><h3></h3><div class='headings'><h5> RUNTIME: " + element.movieInfo.Runtime + "</h5><h5> DIRECTOR: " + element.movieInfo.Director + "</h5><h5> RELEASE DATE: " + element.movieInfo.Released + "</h5></div><div class='button'>WATCH NOW</div><div class='remove-item'><h6>REMOVE FROM WATCHLIST</h6></div></div></div></div>"
+            );
+        });
+    }
+                
+                
+            });
+            
+    
+            
         // create title cards that show the movies using above variables
         
-        if (watchlist){
-     
-     
-         watchlist.forEach(element => {
-              $('.watchListItems').append(
-                "<div class='row'><div class='filmbox col-12 col-md-6 col-lg-4'><div class='picturebox'><img src='" + element.movieInfo.Poster + "'></div><div class='textbox'><h3></h3><div class='headings'><h5> RUNTIME: " + element.movieInfo.Runtime + "</h5><h5> DIRECTOR: " + element.movieInfo.Director + "</h5><h5> RELEASE DATE: " + element.movieInfo.Released + "</h5></div><div class='button'>WATCH NOW</div><div class='remove-item'><h6>REMOVE FROM WATCHLIST</h6></div></div></div></div>"
-            );
-
-     })
- }
-
         
    }
         
