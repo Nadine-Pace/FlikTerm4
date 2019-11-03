cdfeca2d18c2d79a99e2d773fcabdec258f5ac0a
PfanoKhampha <55394528+PfanoKhampha@users.noreply.github.com>
2019-11-03 07:01:20 +0200

    watchlist update

--- js/watch-list.js
+++ js/watch-list.js
@@ -1,57 +1,47 @@
 
 $(function () {
     
-    var apiRequest = new XMLHttpRequest();
-
-    apiRequest.open('GET', 'http://www.omdbapi.com/?i=tt3896198&apikey=d90cffd6', true);
-
-    apiRequest.send();
+var movieName = localStorage.getItem('detailmovieID');
     
-        apiRequest.onload = function(){
-        var watchlist = JSON.parse(localStorage.getItem('watchlist'));
+       // Get API request structure form and put here
+    
+    if (movieName) {
+        var url ='http://www.omdbapi.com/?i=tt3896198&apikey=d90cffd6' + movieName;
+        var apiRequest = new XMLHttpRequest();
         
-                $('.watchListItems').append(
-                "<div class='row'><div class='filmbox col-12 col-md-6 col-lg-4'><div class='picturebox'><img src='" + element.movieInfo.Poster + "'></div><div class='textbox'><h3></h3><div class='headings'><h5> RUNTIME: " + element.movieInfo.Runtime + "</h5><h5> DIRECTOR: " + element.movieInfo.Director + "</h5><h5> RELEASE DATE: " + element.movieInfo.Released + "</h5></div><div class='button'>SEE DETAILS</div><div class='remove-item'><h6>REMOVE</h6></div></div></div></div>"
-            ); 
+        apiRequest.open('GET', url, true);
+        apiRequest.send();
+        apiRequest.onload =function(){
             
-                for(i = 0; i < data.photos.length; i++){
-                 if(data.photos[i].id === storageReference){
-                    
-                    var moviePoster = data.photos[i].Poster;
-                     var movieTitle = data.photos[i].Title;
-                    var movieDirector = data.photos[i].Director;
-                     var movieGenre = data.photos[i].Genre;
-                    var movieRuntime = data.photos[i].Runtime;
-                    var movieReleaseDate = data.photos[i].Released;
-                     
-   
-       }} // For Statment
+         // run the api here with the value (movieTitle) as a variable
+            
+            var movieName = JSON.parse(this.response);
         
-                       var moviePoster = localStorage.getItem('moviePoster');
-           var movieTitle = localStorage.getItem('movieTitle');
-           var movieDirector = localStorage.getItem('movieDirector');
-           var movieRuntime = localStorage.getItem('movieRuntime');
-           var movieReleaseDate = localStorage.getItem('movieReleaseDate');
+            $('.picturebox').attr('src', movie.Poster);
+            $('.picturebox').text(movie.Title);
+            $('.movieDirector').text(movie.Director);
+            $('.movieRuntime').text(movie.Runtime);
+            $('.movieRelease').text(movie.Released);
             
-            $(".picturebox").attr('src', movie.Poster);
-            $(".").text(movie.Title);
-            $("#movieDirector").text(movie.Director);
-            $("#movieRuntime").text(movie.Runtime);
-            $("#movieReleaseDate").text(movie.Released);
-            
+        // create title cards that show the movies using above variables
+        
+        if (watchlist){
+     
+     
+         watchlist.forEach(element => {
+              $('.watchListItems').append(
+                "<div class='row'><div class='filmbox col-12 col-md-6 col-lg-4'><div class='picturebox'><img src='" + element.movieInfo.Poster + "'></div><div class='textbox'><h3></h3><div class='headings'><h5> RUNTIME: " + element.movieInfo.Runtime + "</h5><h5> DIRECTOR: " + element.movieInfo.Director + "</h5><h5> RELEASE DATE: " + element.movieInfo.Released + "</h5></div><div class='button'>WATCH NOW</div><div class='remove-item'><h6>REMOVE FROM WATCHLIST</h6></div></div></div></div>"
+            );
 
+     })
+ }
 
-
-
-            
-            
-            
-            
-            
-            
-            
-            
-        }
+        
+   }
+        
+        
+        
+    }
 
 });
 
