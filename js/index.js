f0fce7fb55bdee590c856a52bac3cadf040a8c1a
Jeandre <190025@virtualwindow.co.za>
2019-11-03 16:39:08 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -27,13 +27,13 @@ if(password === ""){
   $(".validator").text("While FLIK! You're missing something...");
 } else if(password === registeredPasswords[0]){
   localStorage.setItem("activeUser", "SAM");
-  location.href = "http://nadine.pale.co.za/FlikTerm4/"
+  location.href = "pages/home-page.html"
 } else if(password === registeredPasswords[1]){
   localStorage.setItem("activeUser", "JACK");
-  location.href = "http://nadine.pale.co.za/FlikTerm4/"
+  location.href = "pages/home-page.html"
 }else if(password === registeredPasswords[2]){
   localStorage.setItem("activeUser", "BOBBY");
-  location.href ="http://nadine.pale.co.za/FlikTerm4/"
+  location.href ="pages/home-page.html"
 } else{
   $(".validator").text("Your password does not match this profile... ");
 }
